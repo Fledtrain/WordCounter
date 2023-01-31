987f5667c48c5c236c51ec73b6807432b3bfbdc3
Joey <Joeyew2014@gmail.com>
2023-01-31 17:06:51 -0500

    Added Clear button and Changed colors

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ let inputTextArea = document.getElementById("input-textarea");
 let charCount = document.getElementById("char-count");
 let wordCount = document.getElementById("word-count");
 
+
+//Add a function that deletes all the text in the textarea when the clear button is clicked
+document.getElementById("btn-clear").addEventListener("click", () => {
+  inputTextArea.value = "";
+});
+
+//Deletes the local storage when the clear button is clicked and resets the character and word count to 0 
+document.getElementById("btn-clear").addEventListener("click", () => {
+  charCount.textContent = 0;
+  wordCount.textContent = 0;
+  localStorage.clear();
+});
+
+
 inputTextArea.addEventListener("input", () => {
   //Count the number of characters
   charCount.textContent = inputTextArea.value.length;
 
   // Removes the whitespace from both the sides of the string
-
   let txt = inputTextArea.value.trim();
 
-  //SPlit text at spaces into a array of substrings
+  //Split text at spaces into a array of substrings
   //Filter empty strings
   //Count length of this array
   wordCount.textContent = txt.split(" ").filter((str) => str !== "").length;
